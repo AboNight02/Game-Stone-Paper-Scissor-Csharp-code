c71155716d4562a5ab65de34ecf45ccdb9f4a135
Abo Night <ahmdalqhlany@gmail.com>
2024-08-06 11:14:04 +0300

    init files

---
+++ Game/Game/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+
+    enum enGameType
+    {
+        Stone = 1,
+        Paper = 2,
+        Scissor = 3,
+    }
+
+    static void ReadScreen(int resalt)
+    {
+        switch (resalt)
+        {
+            case 0:
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    static void StartGame() { }
+
+
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("hi");
+    }
+}
